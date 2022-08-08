d9774e0dd11351bbbd28ba5dc3ccc906053adadb
Volodkevich Sergey <volodkevich777@gmail.com>
2022-08-08 16:50:19 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ module.exports = function towelSort (matrix) {
                     chek = true;
                     pass[k] = matrix[i][j];
                     k++;
+                    //chek
                 }
             }
         }
